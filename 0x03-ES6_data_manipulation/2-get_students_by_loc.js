61b0906ce6d903331b438583d7bb92681211d7ff
yahya bnoun <yahyaiko22@gmail.com>
2024-08-04 10:30:51 +0100

    1

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,7 +6,7 @@
 *   location: String
 * }[]} students - The list of students.
 * @param {String} city - The location.
-* @author Bezaleel Olakunori <https://github.com/B3zaleel>
+* @author YAHYA BNOUN <https://github.com/yahyabnoun>
 * @returns
 */
 export default function getStudentsByLocation(students, city) {
